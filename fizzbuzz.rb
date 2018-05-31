8eccc33781fb384b4d7fda3e9d85a4ac87bb6c81
Daniel Dunbar <danielqdunbar@gmail.com>
2018-05-31 22:33:05 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,8 @@
 def fizzbuzz(int)
-  if (int % 3 == 0 && int !% 5 == 0)
+  if (int % 3 == 0 && int % 5 != 0)
     "Fizz"
   end
-  if (int !% 3 == 0 && int % 5 == 0)
+  if (int % 3 != 0 && int % 5 == 0)
     "Buzz"
   end
   if (int % 3 == 0 && int % 5 == 0)
